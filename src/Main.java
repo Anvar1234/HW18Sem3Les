93c05ff64cf8e054d62e13e4072663d929083dcd
Anvar <kingartaved@yandex.ru>
2023-04-05 01:16:39 +0500

    Почистил от лишнего.

--- src/Main.java
+++ src/Main.java
@@ -50,33 +50,5 @@ public class Main {
         Task4.symmetricArrayDiff(newFirstArray, newSecondArray);
 
         sc.close();
-
-
-
-
-
-
-
-
-//        ArrayList<Integer> newFirstArray = new ArrayList<>();
-//        newFirstArray = getArrayList.getArray();
-//        System.out.println("Первый рабочий массив: ");
-//        System.out.println(newFirstArray);
-//
-//        Task2.delEvenNumbers(newFirstArray);
-//
-//        Task3.minMaxAverageValue(newFirstArray);
-//
-//        ArrayList<Integer> newSecondArray = new ArrayList<>();
-//        newSecondArray = getArrayList.getArray();
-//        System.out.println("Второй рабочий массив: ");
-//        System.out.println(newSecondArray);
-//        System.out.println("Первый, помним, такой: ");
-//        System.out.println(newFirstArray);
-//
-//        Task4.differenceOfArrays(newFirstArray, newSecondArray);
-//
-//        Task4.symmetricArrayDiff(newFirstArray, newSecondArray);
-
     }
 }
